b0e6dd39dfd561df6e92a73abdf0a98b2e55b564
aadarsh2024 <160094475+aadarsh2024@users.noreply.github.com>
2024-02-15 12:39:31 +0530

    Update project.py

--- project.py
+++ project.py
@@ -7,14 +7,14 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 st.title(" :bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
-fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-     filename = fl.name
-     st.write(filename)
-     df = pd.read_csv(filename)
-else:
-     file_path=(r"C:\Users\Aadarsh\Downloads\Sample - Superstore.xls")
-     df = pd.read_excel(file_path)
+# fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+# if fl is not None:
+#      filename = fl.name
+#      st.write(filename)
+#      df = pd.read_csv(filename)
+# else:
+file_path=(r"Sample - Superstore.xls")
+df = pd.read_excel(file_path)
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
 startDate = pd.to_datetime(df["Order Date"]).min()
