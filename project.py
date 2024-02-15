b9f463a752a758812b8eb197de80e7bc265b77e1
aadarsh2024 <160094475+aadarsh2024@users.noreply.github.com>
2024-02-15 13:14:49 +0530

    Update project.py

--- project.py
+++ project.py
@@ -8,11 +8,11 @@ st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="w
 st.title(" :bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-     filename = fl.name
-     st.write(filename)
-     df = pd.read_csv(filename)
-else:
+     if fl is not None:
+          filename = fl.name
+          st.write(filename)
+          df = pd.read_csv(filename)
+     else:
 file_path=(r"Sample - Superstore.xls")
 df = pd.read_excel(file_path)
 col1, col2 = st.columns((2))
